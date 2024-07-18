677a658f432d4be3459ed7e3a03b7694245de053
Rushikeshchandak30 <chandakrushikesh30@gmail.com>
2024-07-18 23:28:46 +0530

    Changes for images

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -14,7 +14,6 @@ export default async function handler(req, res) {
         const { email, password } = req.body;
         
         const user = await User.findOne({ email });
-        console.log(user)
         if (!user) {
           return res.status(400).json({ success: false, message: 'Invalid credentials' });
         }
