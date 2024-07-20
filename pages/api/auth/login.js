46fe31d6ea898291be2db980b8cfe5171371e9b7
Rushikeshchandak30 <chandakrushikesh30@gmail.com>
2024-07-21 02:37:31 +0530

    errors solved

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -29,7 +29,6 @@ export default async function handler(req, res) {
 
         res.status(200).json({ success: true, token });
       } catch (error) {
-        console.log(error)
         res.status(400).json({ success: false, message: error.message });
       }
       break;
